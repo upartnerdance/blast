ca57508d3ffadb0ff481536ddf326f5390284637
Nicholas Westlake <nicholasredlin@gmail.com>
2013-08-04 05:46:51 -0500

    update build script

--- build.sh
+++ build.sh
@@ -5,6 +5,7 @@ cp -r ./_site /tmp/WORKING_BLAST_SITE
 git checkout gh-pages
 git rm -rf .
 cp -r /tmp/WORKING_BLAST_SITE/* .
+git add -A
 git commit -m 'built site'
 rm -r /tmp/WORKING_BLAST_SITE
 git checkout master
